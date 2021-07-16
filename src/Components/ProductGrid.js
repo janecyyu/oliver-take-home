1af01f8f7f21ac428d9e34f57a17a6edf31bd966
janecyyu <janecyyu@gmail.com>
2021-07-16 15:10:31 -0700

    load prodocts ok

---
+++ src/Components/ProductGrid.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+import Grid from "./Grid";
+import axios from "axios";
+
+export default function ProductGrid() {
+  const [products, setProducts] = useState();
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await axios("http://localhost:3004/products");
+      setProducts(response.data);
+      setLoading(false);
+    }
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      {products.map((product) => (
+        <Grid key={product.id} />
+      ))}
+    </div>
+  );
+}
