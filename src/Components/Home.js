af7dc0043b038328d8a77b9c2d1eed3912cd1aa1
janecyyu <janecyyu@gmail.com>
2021-07-16 14:35:05 -0700

    on local

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,60 +1,72 @@
-import React from 'react';
-import '../Styles/Home.scss';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import ButtonBase from "@material-ui/core/ButtonBase";
 
-class Home extends React.Component {
-  render() {
-    return (
-        <div className="Home">
-          <img
-            src="https://res.cloudinary.com/hwery4evq/image/upload/c_scale,w_400/v1/assets/toast"
-            alt="welcome"/>
-          <h1>
-            Welcome!
-          </h1>
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    margin: "auto",
+    maxWidth: 200,
+  },
+  image: {
+    width: 128,
+    height: 128,
+  },
+  img: {
+    margin: "auto",
+    display: "block",
+    maxWidth: "100%",
+    maxHeight: "100%",
+  },
+}));
 
-          <h2>
-            Oliver Space Take-Home Instructions
-          </h2>
-          <p>
-            Your mission, if you choose to accept it, is to build out a mock of a product reviews flow. 
-            This includes a page for users to submit their reviews as well as a page to read reviews.
-          </p>
-          <h3>Requirements</h3>
-          <ul>
-            <li>Create a page where a user can review a given product with the following information:</li>
-              <ul>
-                <li>Their name (the author)</li>
-                <li>A star rating</li>
-                <li>Headline text (a title for their review)</li>
-                <li>The body of the review (a longer paragraph)</li>
-              </ul>
-              <li>Create a page where a user can view all of the reviews for a given product</li>
-            </ul>
-          <p>
-            Please spend 90 minutes completing this task to whatever extent you can finish, but don't go over time. 
-            The expectation is not that you will get through all of the requirements, so please add notes on anything 
-            else you would have done with more time. Feel free to use any third-party libraries that you find helpful.
-          </p>
+export default function ComplexGrid() {
+  const classes = useStyles();
 
-          <h3>Running the App</h3>
-          <ul>
-            <li>Run <code>npm install</code> first</li>
-            <li>Start the mock JSON server on port 3004 using <code>json-server --watch db.json</code></li>
-            <li>Start the React app on port 3000 using <code>npm start</code></li>
-            <li>You can then make requests to <code>http://localhost:3004/products</code>, <code>http://localhost:3004/reviews</code> or <code>http://localhost:3004/products/:id/reviews</code> using axios, or any of your preferred methods</li>
-          </ul>
-          <p>More info on json-server <a href="https://www.npmjs.com/package/json-server">here</a></p>
-
-          <p>This project was bootstrapped with <a href="https://github.com/facebook/create-react-app">Create React App</a></p>
-
-          <h4>
-            Best of luck! Feel free to email <a href="mailto:caroline@oliver.space">caroline@oliver.space</a>
-            or text (203) 434 - 2075 if you have any questions while you're completing the exercise. I'm
-            happy to help :)
-          </h4>
-        </div>
-      );
-  }
+  return (
+    <div className={classes.root}>
+      <Paper className={classes.paper}>
+        <Grid container spacing={2}>
+          <Grid item>
+            <ButtonBase className={classes.image}>
+              <img
+                className={classes.img}
+                alt="complex"
+                src="/static/images/grid/complex.jpg"
+              />
+            </ButtonBase>
+          </Grid>
+          <Grid item xs={12} sm container>
+            <Grid item xs container direction="column" spacing={2}>
+              <Grid item xs>
+                <Typography gutterBottom variant="subtitle1">
+                  Standard license
+                </Typography>
+                <Typography variant="body2" gutterBottom>
+                  Full resolution 1920x1080 • JPEG
+                </Typography>
+                <Typography variant="body2" color="textSecondary">
+                  ID: 1030114
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography variant="body2" style={{ cursor: "pointer" }}>
+                  Remove
+                </Typography>
+              </Grid>
+            </Grid>
+            <Grid item>
+              <Typography variant="subtitle1">$19.00</Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Paper>
+    </div>
+  );
 }
-
-export default Home;
